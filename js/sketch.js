c0713b69e61b988c2e832d121bf41042f123533c
Keith Browning <keithbarrettbrowning@gmail.com>
2017-08-03 17:52:19 -0400

    16 x 16 grid that can be painted and cleared

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-$('p').click(function(){
-      $('p').hide();
 
+$(function() {
+    for (var x = 0; x < 16; x++) {
+        for (var y = 0; y < 16; y++) {
+            $("<div>").addClass("unit").appendTo(".grid");
+                 }
+             }
+         });
+
+$(document).on('mouseenter', '.unit', function() {
+        $(this).addClass('hovered');
+        });
+
+$('.clear').click(function(){
+  $('.unit').removeClass('hovered');
+  $('.clear').toggleClass('pcolor');
 });
+
+
 
 });
